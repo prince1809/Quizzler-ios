6195d0af22a176928b12b99497517ed8c0575610
prince <prince18pat@gmail.com>
2019-08-17 18:56:04 +0900

    add question model

--- Quizzler/AppDelegate.swift
+++ Quizzler/AppDelegate.swift
@@ -1,11 +1,3 @@
-//
-//  AppDelegate.swift
-//  Quizzler
-//
-//  Created by Angela Yu on 25/08/2015.
-//  Copyright (c) 2015 London App Brewery. All rights reserved.
-//
-
 import UIKit
 
 @UIApplicationMain
