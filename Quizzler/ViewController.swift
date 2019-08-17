dfc6633249b2536c46b38be1eef97006fb2586c8
prince <prince18pat@gmail.com>
2019-08-17 18:26:49 +0900

    project created with basic

--- Quizzler/ViewController.swift
+++ Quizzler/ViewController.swift
@@ -2,19 +2,52 @@
 //  ViewController.swift
 //  Quizzler
 //
-//  Created by Prince Kumar on 2019/08/17.
-//  Copyright © 2019 Prince Kumar. All rights reserved.
+//  Created by Angela Yu on 25/08/2015.
+//  Copyright (c) 2015 London App Brewery. All rights reserved.
 //
 
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //Place your instance variables here
+    
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var scoreLabel: UILabel!
+    @IBOutlet var progressBar: UIView!
+    @IBOutlet weak var progressLabel: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
     }
 
 
-}
+    @IBAction func answerPressed(_ sender: AnyObject) {
+  
+    }
+    
+    
+    func updateUI() {
+      
+    }
+    
 
+    func nextQuestion() {
+        
+    }
+    
+    
+    func checkAnswer() {
+        
+    }
+    
+    
+    func startOver() {
+       
+    }
+    
+
+    
+}
